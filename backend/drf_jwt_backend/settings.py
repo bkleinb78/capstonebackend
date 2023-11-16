166899337a84eea947081714aa7e2a9440e80253
bkleinb78 <y2bk728@aol.com>
2023-11-16 09:58:32 -0600

    fixed a correction with the local settings

--- backend/drf_jwt_backend/settings.py
+++ backend/drf_jwt_backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 AUTH_USER_MODEL = 'authentication.User'
 
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    
+
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
